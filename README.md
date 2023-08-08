72a3ee2155f9d8cf663d4bee393ba801b7d4087c
kalikutta <iverin91@gmail.com>
2023-08-08 20:48:10 +0200

    bro

---
+++ README.md
@@ -0,0 +1,2 @@
+Send Mail with uipath with additional settings, for example
+options: Do not forward, encrypt and more.
---
+++ Send_Mail.cs
@@ -0,0 +1,42 @@
+﻿using System.Activities;
+using System.ComponentModel;
+
+
+namespace Kali_Mail
+{
+    public class Send_Mail : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        [Description("Enter recipient mail")]
+        [DisplayName("To")]
+        public InArgument<string> To { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        [Description("Enter mailbody")]
+        [DisplayName("Body")]
+        public InArgument<string> Body { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        [Description("Enter subject")]
+        [DisplayName("Subject")]
+        public InArgument<string> Subject { get; set; }
+
+        [Category("Input")]
+        [Description("Is the body HTML?")]
+        [DisplayName("Is HTML")]
+        public InArgument<bool> IsHtml { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            string recipient = To.Get(context);
+            string mailBody = Body.Get(context);
+            string mailSubject = Subject.Get(context);
+            bool isHtml = IsHtml.Get(context);
+
+
+        }
+    }
+}
