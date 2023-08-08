1a87296a85003fb20736e92b13ef7fc63114983e
kalikutta <iverin91@gmail.com>
2023-08-08 21:43:20 +0200

    work in progress

--- Send_Mail.cs
+++ Send_Mail.cs
@@ -1,9 +1,6 @@
 ﻿using System.Activities;
 using System.ComponentModel;
-
-
-
-
+using Microsoft.Office.Interop.Outlook;
 
 namespace Kali_Mail
 {
@@ -32,12 +29,45 @@ namespace Kali_Mail
         [DisplayName("Is HTML")]
         public InArgument<bool> IsHtml { get; set; }
 
+        [Category("Input")]
+        [Description("Do not forward")]
+        [DisplayName("Do not forward")]
+        public InArgument<bool> doNotFoward { get; set; }
+
         protected override void Execute(CodeActivityContext context)
         {
             string recipient = To.Get(context);
             string mailBody = Body.Get(context);
             string mailSubject = Subject.Get(context);
             bool isHtml = IsHtml.Get(context);
+            bool dnf = doNotFoward.Get(context);
+
+            // Create a new Outlook application.
+            Application outlookApp = new Application();
+
+            // Create a new MailItem.
+            MailItem mailItem = outlookApp.CreateItem(OlItemType.olMailItem) as MailItem;
+
+            // Set recipient, subject, and body.
+            mailItem.To = recipient;
+            mailItem.Subject = mailSubject;
+            
+            if (isHtml)
+                mailItem.BodyFormat = OlBodyFormat.olFormatHTML;
+            else
+                mailItem.BodyFormat = OlBodyFormat.olFormatPlain;
+            
+            // Set the permission to do not forward.
+            if (dnf)
+                mailItem.Permission = OlPermission.olDoNotForward;
+            
+
+            // Send the email.
+            mailItem.Send();
+
+            // Clean up.
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(mailItem);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(outlookApp);
 
 
         }
